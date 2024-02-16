2df0ede2dd32476c0d8aa845b028155d6df63496
zihco <chizobajames21@gmail.com>
2024-02-16 15:04:47 +0100

    done

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""script contains the Auth class"""
+from flask import request
+
+
+class Auth:
+    """implements Basic authentication"""
+    def require_auth(self, path: str, excluded_paths: List(str)) -> bool:
+        """authentication"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user"""
+        return None
