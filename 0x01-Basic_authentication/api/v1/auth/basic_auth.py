dcfb05b1578a0c346a1c6ce2bf1d09a2a6f0d3fc
zihco <chizobajames21@gmail.com>
2024-02-17 13:44:42 +0100

    done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -16,3 +16,18 @@ class BasicAuth(Auth):
         if authorization_header[0:6] != "Basic ":
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """implement base64 encoding"""
+        import base64
+
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header)
+        except Exception:
+            return None
