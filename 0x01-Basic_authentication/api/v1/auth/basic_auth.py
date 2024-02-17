e75e7186f0e4a740c1a2ad97b785a5744bcc16dc
zihco <chizobajames21@gmail.com>
2024-02-17 17:48:32 +0100

    done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -61,7 +61,7 @@ class BasicAuth(Auth):
             return None
         try:
             student = User.search({'email': user_email})
-            student[0].is_valid_password(user_pwd)
-            return student[0]
+            if student[0].is_valid_password(user_pwd):
+                return student[0]
         except Exception:
             return None
