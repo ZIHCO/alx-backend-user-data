7b2d4b017ac729c76261bc9db8b792a3e1995cd2
zihco <chizobajames21@gmail.com>
2024-02-19 19:24:00 +0100

    session auth

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -37,7 +37,7 @@ class SessionExpAuth(SessionAuth):
         created_at = session_dictionary.get("created_at")
         if not created_at:
             return None
-        if ((created_at.seconds + self.session_duration) <
+        if ((created_at.second + self.session_duration) <
            datetime.timedelta().seconds):
             return None
         return session_dictionary.get("user_id")
