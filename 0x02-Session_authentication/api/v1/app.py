4ada642791cb062892dbdad68135fdf59752eba6
zihco <chizobajames21@gmail.com>
2024-02-19 10:06:43 +0100

    session auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -62,7 +62,6 @@ def before_request():
             if not auth.authorization_header(request):
                 if not auth.session_cookie(request):
                     abort(401)
-                abort(401)
             if not auth.current_user(request):
                 abort(403)
         request.current_user = auth.current_user(request)
