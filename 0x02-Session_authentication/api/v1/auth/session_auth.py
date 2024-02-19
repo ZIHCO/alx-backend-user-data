005e956894f6d320085a75ffa534339a214daa4d
zihco <chizobajames21@gmail.com>
2024-02-19 10:23:52 +0100

    session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -34,4 +34,4 @@ class SessionAuth(Auth):
         session_cookie = self.session_cookie(request)
         session_id = session_cookie.get("session_id")
         user_id_for_session_id = self.user_id_for_session_id(session_id)
-        return User.get(user_id_for_session_id)
+        return User.get(user_id=user_id_for_session_id)
