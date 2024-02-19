292ebcaba2282fd8130b2ed2bc10df595962bc61
zihco <chizobajames21@gmail.com>
2024-02-19 10:52:55 +0100

    session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -29,12 +29,9 @@ class SessionAuth(Auth):
         user_id = type(self).user_id_by_session_id.get(session_id)
         return user_id
 
-    def current_user(Self, request=None):
+    def current_user(self, request=None):
         """return a User instance based on the cookies"""
         session_cookie = self.session_cookie(request)
-        if not session_cookie:
-            return None
         session_id = session_cookie.get("session_id")
         user_id_for_session_id = self.user_id_for_session_id(session_id)
-        if user_id_for_session_id:
-            return User.get(user_id=user_id_for_session_id)
+        return User.get(user_id=user_id_for_session_id)
