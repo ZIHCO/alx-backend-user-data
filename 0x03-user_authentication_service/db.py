3b5d5ef15aff03a22b1d444a95bf833a406a69f2
zihco <chizobajames21@gmail.com>
2024-02-20 19:02:33 +0100

    user model

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,10 +33,10 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
         """add a user to DB"""
-        if ((email and hashed_password) and
-           (type(email) is str and type(hashed_password) is str)):
-            kwargs = {"email": email, "hashed_password": hashed_password}
-            user = User(**kwargs)
-            self._session.add(user)
-            self._session.commit()
-            return user
+        """if ((email and hashed_password) and
+           (type(email) is str and type(hashed_password) is str)):"""
+        kwargs = {"email": email, "hashed_password": hashed_password}
+        user = User(**kwargs)
+        self._session.add(user)
+        self._session.commit()
+        return user
