3a2576329fa2936d1a70fcf42c86a07690c0dd3b
zihco <chizobajames21@gmail.com>
2024-02-24 05:53:24 +0100

    hashed password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """authentication implimented"""
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -8,3 +10,24 @@ def _hash_password(password: str) -> bytes:
     if password:
         salt = bcrypt.gensalt()
         return bcrypt.hashpw(password.encode(), salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """impliment user registration"""
+        if email and password:
+            kwargs = {"email": email}
+            try:
+                user = self._db.find_user_by(**kwargs)
+                if user:
+                    raise ValueError(f"User {email} already exists")
+            except Exception:
+                password = _hash_password(password)
+                user = self._db.add_user(email, password)
+                return user
